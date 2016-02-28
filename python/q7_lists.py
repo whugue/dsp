ee9a3e47177944fe47ee53380751207935046b90
Will Huguenin <willhuguenin@Will-Huguenins-MacBook-Pro-2.local>
2016-02-28 17:16:41 -0500

    First Function

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -2,20 +2,25 @@
 # Licensed under the Apache License, Version 2.0
 
 
-def match_ends(words):
-    """
-    Given a list of strings, return the count of the number of strings
-    where the string length is 2 or more and the first and last chars
-    of the string are the same.
+"""
+Given a list of strings, return the count of the number of strings
+where the string length is 2 or more and the first and last chars
+of the string are the same.
+"""
 
-    >>> match_ends(['aba', 'xyz', 'aa', 'x', 'bbb'])
-    3
-    >>> match_ends(['', 'x', 'xy', 'xyx', 'xx'])
-    2
-    >>> match_ends(['aaa', 'be', 'abc', 'hello'])
-    1
-    """
-    raise NotImplementedError
+def match_ends(words):
+    count=0
+
+    for word in words:
+        if len(word) >= 2 and word[0]==word[len(word)-1]:
+            count +=1
+
+    return count
+
+#Test Functions
+print match_ends(['aba', 'xyz', 'aa', 'x', 'bbb'])
+print match_ends(['', 'x', 'xy', 'xyx', 'xx'])
+print match_ends(['aaa', 'be', 'abc', 'hello'])
 
 
 def front_x(words):
