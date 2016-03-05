3da7a4b0bb5d207cf9b9b51b1356f27fcb23caff
Will Huguenin <willhuguenin@Will-Huguenins-MacBook-Pro-2.local>
2016-03-05 13:56:14 -0500

    Answered Datetime Qs

--- 05a-python.md
+++ 05a-python.md
@@ -51,7 +51,7 @@ date_start = '01-02-2013'
 date_stop = '07-28-2015'
 ```
 
->> REPLACE THIS TEXT WITH YOUR RESPONSE (answer will be in number of days)
+There are 937 days between 1/2/2013 and 7/28/2015
 
 b.  
 ```
@@ -59,7 +59,7 @@ date_start = '12312013'
 date_stop = '05282015'  
 ```
 
->> REPLACE THIS TEXT WITH YOUR RESPONSE (answer will be in number of days)
+There are 513 days between 12/31/2013 and 05/28/2015
 
 c.  
 ```
@@ -67,7 +67,8 @@ date_start = '15-Jan-1994'
 date_stop = '14-Jul-2015'  
 ```
 
->> REPLACE THIS TEXT WITH YOUR RESPONSE  (answer will be in number of days)
+There are 7,850 days betwene 1/15/1994 and 7/14/2015
+
 
 Place code in this file: [q5_datetime.py](python/q5_datetime.py)
 
--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,17 +1,26 @@
 # Hint:  use Google to find python function
 
-####a) 
-date_start = '01-02-2013'
-date_stop = '07-28-2015'
+from datetime import datetime
+
+###a)
+date_start = "01-02-2013"
+date_stop = "07-28-2015"
+
+print (datetime.strptime(date_stop,"%m-%d-%Y")-datetime.strptime(date_start,"%m-%d-%Y")).days #937 days
 
 
+###b)
+date_start = "12312013"
+date_stop = "05282015"
+
+print (datetime.strptime(date_stop,"%m%d%Y")-datetime.strptime(date_start,"%m%d%Y")).days #513 days
 
 
-
-####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
-
-####c)  
+###c)
 date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+date_stop = '14-Jul-2015'
+
+print (datetime.strptime(date_stop,"%d-%b-%Y")-datetime.strptime(date_start,"%d-%b-%Y")).days #7850 days
+
+
+
