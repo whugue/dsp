5f22d1e6733d9e0f5b9e710950896462d17f1b6a
Will Huguenin <willhuguenin@Will-Huguenins-MacBook-Pro-2.local>
2016-03-10 20:55:28 -0500

    write to csv

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,18 @@
-PLACE YOUR CODE HERE
+
+#Read In Data;
+import csv
+faculty=list(csv.reader(open("faculty.csv")))
+
+
+#Write Out Emails to CSV Files
+out=open("emails.csv", "w")
+
+for i in range(0,len(faculty)):
+	out.write(faculty[i][3] + "\n")
+
+out.close()
+
+
+
+
+
