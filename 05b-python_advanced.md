9e38cd7967db051a7547f49a766d02e61d4fcc39
Will Huguenin <willhuguenin@Will-Huguenins-MacBook-Pro-2.local>
2016-03-16 09:28:05 -0400

    Dict Qs

--- 05b-python_advanced.md
+++ 05b-python_advanced.md
@@ -78,7 +78,10 @@ faculty_dict = { 'Ellenberg': [\
 ```
 Print the first 3 key and value pairs of the dictionary:
 
->> REPLACE THIS WITH YOUR RESPONSE
+>>
+```
+[('Putt', [['PhD ScD', 'Professor', 'mputt@mail.med.upenn.edu']]), ('Feng', [['Ph.D', 'Assistant Professor', 'ruifeng@upenn.edu']]), ('Bilker', [['Ph.D.', 'Professor', 'warren@upenn.edu']])]
+```
 
 ####Q7.  The previous dictionary does not have the best design for keys.  Create a new dictionary with keys as:
 
@@ -93,11 +96,17 @@ professor_dict = {('Susan', 'Ellenberg'): ['Ph.D.', 'Professor', 'sellenbe@upenn
 
 Print the first 3 key and value pairs of the dictionary:
 
->> REPLACE THIS WITH YOUR RESPONSE
+>>
+```
+[(('Jinbo', 'Chen'), ['Ph.D.', 'Associate Professor', 'jinboche@upenn.edu']), (('Scarlett', 'Bellamy'), ['Sc.D.', 'Associate Professor', 'bellamys@mail.med.upenn.edu']), (('Susan', 'Ellenberg'), ['Ph.D.', 'Professor', 'sellenbe@upenn.edu'])]
+```
 
 ####Q8.  It looks like the current dictionary is printing by first name.  Sort by last name and print the first 3 key and value pairs.  
 
->> REPLACE THIS WITH YOUR RESPONSE
+>>
+```
+[(('Scarlett', 'Bellamy'), ['Sc.D.', 'Associate Professor', 'bellamys@mail.med.upenn.edu']), (('Warren', 'Bilker'), ['Ph.D.', 'Professor', 'warren@upenn.edu']), (('Matthew', 'Bryan'), ['PhD', 'Assistant Professor', 'bryanma@upenn.edu'])]
+```
 
 Place your code in this file: [advanced_python_dict.py](python/advanced_python_dict.py)
 
--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -2,45 +2,55 @@
 #Read CSV data into nested list
 import csv
 import re
+from collections import OrderedDict
 
 faculty=list(csv.reader(open("faculty.csv")))
+
 
 #Create faculty_dict dictionary
 def create_dict_1(data):
 	faculty_dict={}
 
-	for i in range(1,len(data)):
-		names=re.split(" ",data[i][0],flags=re.I)
-		last_name=names[len(names)]
-
-		degree_type=data[i][1]
+	for i in range(1,len(data)): #Start at index 1 to skip over variable names
+		name_list=re.split(" ",data[i][0],flags=re.I)
+		last_name=name_list[len(name_list)-1]
+		
+		degree_type=data[i][1].strip()
 		job_title=re.sub(" is "," of ",data[i][2],flags=re.I)
 		job_title=re.split(" of",job_title,flags=re.I)[0]
 		email=data[i][3]
-
 		faculty_info=[degree_type, job_title, email]
 
 		if last_name not in faculty_dict:
-			faculty_dict[last_name]=faculty_info
+			faculty_dict[last_name]=[faculty_info]
+
 		else:
-			faculty_dict[last_name]=faculty_dict[last_name].append(faculty_info) #will this work?
+			temp_list=[]
+			for i in range(0,len(faculty_dict[last_name])):
+				temp_list.append(faculty_dict[last_name][i])
+
+			temp_list.append(faculty_info)
+
+			faculty_dict[last_name]=temp_list
 
 	return faculty_dict
 
-print create_dict_1(faculty)
+faculty_dict=create_dict_1(faculty)
+
+print faculty_dict.items()[0:3] #Print first three key-value pairs (as tuples) only
 
 
 #Create professor_dict dictionary
 def create_dict_2(data):
 	professor_dict={}
 
-	for i in range(1,len(data)):
+	for i in range(1,len(data)): #replace with full range later
 		name_list=re.split(" ",data[i][0],flags=re.I)
 		first_name=name_list[0]
-		last_name=name_list[len(name_list)]
+		last_name=name_list[len(name_list)-1]
 		full_name=(first_name, last_name)
 
-		degree_type=data[i][1]
+		degree_type=data[i][1].strip()
 		job_title=re.sub(" is "," of ",data[i][2],flags=re.I)
 		job_title=re.split(" of",job_title,flags=re.I)[0]
 		email=data[i][3]
@@ -53,12 +63,14 @@ def create_dict_2(data):
 
 	return professor_dict
 
-print create_dict_2(faculty)
+professor_dict=create_dict_2(faculty)
+
+print professor_dict.items()[0:3] #print first three key-value parings (as tuples) only
 
 
+#Sort professor_dict dictonary
+prof_dict_ordered=OrderedDict(sorted(professor_dict.items(), key=lambda x: x[0][1]))
 
-
-
-
+print prof_dict_ordered.items()[0:3] #Print first three key-value pairs of ordered dictionary
 
 
